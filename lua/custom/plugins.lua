6dbe30e1ffa808de001dadae31f8511aa823600a
YanTree <yantreeland@outlook.com>
2023-11-14 22:31:16 +0800

    add simple rust dev env

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -2,6 +2,26 @@ local plugins = {}
 
 plugins = {
   {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+      },
+    },
+  },
+  -----------------------------------rust--------------------------------------
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require('rust-tools').setup(opts)
+    end
+  },
+  {
     "tpope/vim-fugitive",
     cmd = { "Git", "G",},
   }
